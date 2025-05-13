afbb0940358f8da9813a6da6588cb42cca42ffb8
sagiga80 <sagiga@gmail.com>
2025-05-13 12:46:32 +0300

    Week1 / Day3 / daily challange

--- Week1/Day3/DailyChallenge/Daily_challange.py
+++ Week1/Day3/DailyChallenge/Daily_challange.py
@@ -1,20 +1,24 @@
 # Challenge 1: Letter Index Dictionary
-# Goal: Create a dictionary that stores the indices (number of the position) of each letter in a word provided by the user(input()).
+# Goal: Create a dictionary of indices of each letter in a user input.
 
-# 1. User Input:
-# Ask the user to enter a word.
-# Store the input word in a variable.
-# 2. Creating the Dictionary:
-# Iterate through each character of the input word using a loop.
-# And check if the character is already a key in the dictionary.
+# Iterate through characters and check if each character is already a key in the dictionary.
 # If it is, append the current index to the list associated with that key.
 # If it is not, create a new key-value pair in the dictionary.
 # Ensure that the characters (keys) are strings.
 # Ensure that the indices (values) are stored in lists.
-# 3. Expected Output:
-# For the input “dodo”, the output should be: {"d": [0, 2], "o": [1, 3]}.
-# For the input “froggy”, the output should be: {"f": [0], "r": [1], "o": [2], "g": [3, 4], "y": [5]}.
-# For the input “grapes”, the output should be: {"g": [0], "r": [1], "a": [2], "p": [3], "e": [4], "s": [5]}.
+
+user_word = input("Please enter a word: \n")
+letter_indices = {}
+
+# Iterate over the word with index
+for index, letter in enumerate(user_word):
+    if letter in letter_indices:
+        letter_indices[letter].append(index)
+    else:
+        letter_indices[letter] = [index]
+
+print("Letter Index Dictionary:\n", letter_indices)
+print("\n")
 
 
 # Challenge 2: Affordable Items
